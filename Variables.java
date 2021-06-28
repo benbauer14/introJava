9083d2e71e2fa34a0db8e5f3fd3b3a13d8c75f69
Ben Bauer <ben.bauer1@gmail.com>
2021-06-28 12:49:00 -0500

    intial commit

---
+++ Variables.java
@@ -0,0 +1,29 @@
+//This is the class declaration
+public class Variables {
+    //This is the main method that runs when you compile
+      public static void main(String[] args) {
+      //This is where you will define your variable
+
+      //string holds strings
+      String creator = "James Gosling";
+      //int holds whole numbers
+      int numComments = 6;
+      //double holds decimals and very large or small numbers
+      double androidShare = 81.7;
+      //boolean
+      boolean javaIsACompiledLanguage = true;
+      //char holds a single character value. It can be a number, a letter or space. Value assigned is surrounded by single quotes.
+      char expectedGrade = 'A';
+
+      //This is where you will print your variable
+      System.out.println(creator);
+      System.out.println(numComments);
+      System.out.println(androidShare);
+      System.out.println(javaIsACompiledLanguage);
+      System.out.println(expectedGrade);
+      }
+    
+    //This is the end of the class
+  }
+  
+  //This is outside the class
