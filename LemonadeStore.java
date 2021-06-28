e3bfc8a947caf6e56ca1b2e10f7718ba18574d57
Ben Bauer <ben.bauer1@gmail.com>
2021-06-28 14:05:17 -0500

    added example using return

---
+++ LemonadeStore.java
@@ -0,0 +1,29 @@
+public class LemonadeStore {
+    // methods on objects with return value
+    String productType;
+    double price;
+    
+    // constructor method
+    public LemonadeStore(String product, double initialPrice) {
+      productType = product;
+      price = initialPrice;
+    }
+    
+    // increase price method
+    public void increasePrice(double priceToAdd){
+      double newPrice = price + priceToAdd;
+      price = newPrice;
+    }
+    
+    // get price with tax method
+    public double getPriceWithTax(){
+      double totalPrice = price + price * 0.08;
+      return totalPrice;
+    }
+    // main method
+    public static void main(String[] args) {
+      LemonadeStore lemonadeStand = new LemonadeStore("Lemonade", 3.75);
+      double lemonadePrice = lemonadeStand.getPriceWithTax();
+      System.out.println(lemonadePrice);
+    }
+  }
--- README.md
+++ README.md
@@ -3,3 +3,4 @@ This repo contains introductory Java examples.
 1) Creator - Basic layout of a java file with print lines.
 2) Variables - Basic variable convention and naming
 3) Store - Object creation and method execution on objects
+4) LemonadeStore - Object creation with method returning values
