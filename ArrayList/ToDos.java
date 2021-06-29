51b633f5bbe7898bdf08b2571f606b5b225b227a
Ben Bauer <ben.bauer1@gmail.com>
2021-06-29 14:00:39 -0500

    ArrayList example created.

--- ArrayList/ToDos.java
+++ ArrayList/ToDos.java
@@ -17,12 +17,21 @@ class ToDos {
     sherlocksToDos.add("listen to Dr. Watson for amusement");
     sherlocksToDos.add("solve the case");
     sherlocksToDos.add("apprehend the criminal");
+
+    //edits value in index 1
+    sherlocksToDos.set(1, "Dance slowly")
     
     //removes item from arraylist
     sherlocksToDos.remove("visit the crime scene");
+
+    //retrieves value from specified index
+    System.out.println(sherlocksToDos.get(2)); 
     
     // Calculate to-dos until case is solved:
     System.out.println(sherlocksToDos.indexOf("solve the case"));
+
+    //number of index in arraylist
+    System.out.println(sherlocksToDos.size());
       
     // Change the value printed:
     System.out.println("PRINT THE ANSWER HERE");
