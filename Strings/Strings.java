c3f2b7fa0bd380b0860a595865f88d426631530e
Ben Bauer <ben.bauer1@gmail.com>
2021-07-07 13:28:50 -0500

    added to string

--- Strings/Strings.java
+++ Strings/Strings.java
@@ -1,12 +1,13 @@
 //This file contains string manipulation commands in Java
 
-.toUpperCase()
-.toLowerCase()
-
 str.toUpperCase(); //makes a string uppercase
+str.toLowerCase(); //makes a string lowercase
 
 str.equals("Text or Variable") // evaluates whether a string matches
 str.equalsIgnoreCase("string") // evaluates whether a string matches regarless of case
 
 str.trim() //removes spaces after the final character
+
+str.parseInt() //changes a string of numbers to an int
+
 
